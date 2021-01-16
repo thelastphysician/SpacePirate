cc8c10f027807ea23d969b7cfdb328acb688d79d
Matthew John Stepita <mattomg24@gmail.com>
2021-01-16 10:21:58 -0800

    First Push

---
+++ SpacePirate/Assets/GameData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/GameData", order = 1)]
+public class GameData : ScriptableObject
+{
+    public int Score = 0;
+    public int Hull = 3;
+    public float Energy = 100f;
+}
